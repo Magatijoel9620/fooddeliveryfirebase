a2b063c04974270b0bd455c34a827ee0a64ca304
Magatijoel9620 <joeldamarko@gmail.com>
2025-07-02 14:41:01 +0300

    Upgrade

--- android_studio/app/src/main/kotlin/com/example/food_delivery_firebase/MainActivity.kt
+++ android_studio/app/src/main/kotlin/com/example/food_delivery_firebase/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.food_delivery_firebase
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
